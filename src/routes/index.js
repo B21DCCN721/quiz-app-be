12def814cbe1c30074f25f963c550fa4b51b1a94
Geralt-t <nguyenquangduy203@gmail.com>
2025-05-03 22:12:17 +0700

    Tạo nhánh duy-branch và update history api

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@ const authRoute = require('./auth.route');
 const teacherRoute = require('./teacher.route');
 const studentRoute = require('./student.route');
 const exerciseRoute = require('./exercise.route');
-
+const historyRoute = require('./history.route');
 function route(app) {
     app.use('/api/auth', authRoute);
     app.use('/api/teacher', teacherRoute);
     app.use('/api/student', studentRoute);
     app.use('/api/exercises', exerciseRoute);
+    app.use('/api/history', historyRoute);
 }
 
 module.exports = route;
